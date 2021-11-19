17850f6396c323c99534561d12c46d83fbed7fc0
Nishant <getrooted0019@hotmail.com>
2021-11-19 22:08:23 +0530

    Implimented long press shortcuts.

--- src/app/src/main/java/com/nishant/dev/todolist/MainActivity.kt
+++ src/app/src/main/java/com/nishant/dev/todolist/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.nishant.dev.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 import androidx.fragment.app.Fragment
 import androidx.room.Room
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,6 +15,8 @@ import com.nishant.dev.todolist.bottomNavigationFragments.todoFragment.TodoFragm
 import com.nishant.dev.todolist.database.ToDoDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private var defaultFragment = "in progress"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -34,16 +40,18 @@ class MainActivity : AppCompatActivity() {
         val todoFragment = TodoFragment(todoDao)
         val doingFragment = InProgressFragment(todoDao)
 
+        // Set listener for bottom nav bar.
+        val navBar = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
         // Initialize activity by setting the default launch fragment to
         // TodoFragment
         supportFragmentManager.beginTransaction()
             .replace(R.id.bottom_nav_fragment_container, doingFragment)
             .commit()
 
-        // Set listener for bottom nav bar.
-        val navBar = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-
         navBar.selectedItemId = R.id.in_progress_bottom_nav
+
+        longPressShortcutListener(doneFragment, todoFragment, doingFragment, navBar)
 
         navBar.setOnItemSelectedListener { menuItem ->
             when(menuItem.itemId) {
@@ -62,6 +70,10 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
+
+        setLongPressShortcuts()
+
     }
 
     private fun setFragment(fragment: Fragment): Boolean {
@@ -71,4 +83,93 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
+    private fun setLongPressShortcuts() {
+
+        val todoShortcut = ShortcutInfoCompat.Builder(this, "todoShortcut")
+            .setShortLabel("Todo")
+            .setLongLabel("Open todo tasks list.")
+            .setIcon(IconCompat.createWithResource(this, R.drawable.ic_outline_undo_24_black))
+            .setIntent(
+                Intent(this, MainActivity::class.java).setAction("todo")
+            )
+            .build()
+
+        val inProgressShortcut = ShortcutInfoCompat.Builder(this, "inProgressShortcut")
+            .setShortLabel("In Progress")
+            .setLongLabel("Open in progress tasks.")
+            .setIcon(IconCompat.createWithResource(this, R.drawable.ic_outline_undo_24_black))
+            .setIntent(
+                Intent(this, MainActivity::class.java).setAction("in progress")
+            )
+            .build()
+
+        val doneShortcut = ShortcutInfoCompat.Builder(this, "doneShortcut")
+            .setShortLabel("Done")
+            .setLongLabel("Open completed tasks.")
+            .setIcon(IconCompat.createWithResource(this, R.drawable.ic_outline_undo_24_black))
+            .setIntent(
+                Intent(this, MainActivity::class.java).setAction("done")
+            )
+            .build()
+
+
+        val listTest :MutableList<ShortcutInfoCompat> = ArrayList()
+        listTest.add(todoShortcut)
+        listTest.add(inProgressShortcut)
+        listTest.add(doneShortcut)
+
+        ShortcutManagerCompat.setDynamicShortcuts(this, listTest)
+    }
+
+    private fun longPressShortcutListener(
+        doneFragment: DoneFragment,
+        todoFragment: TodoFragment,
+        doingFragment: InProgressFragment,
+        navBar: BottomNavigationView
+    ) {
+
+        val action = if (intent != null) intent.action else null
+        if (action != null) {
+
+            when (action) {
+                "todo" -> {
+
+                    // Set title.
+                    supportActionBar?.title = "Todo"
+
+                    // Set fragment.
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.bottom_nav_fragment_container, todoFragment)
+                        .commit()
+
+                    navBar.selectedItemId = R.id.todo_bottom_nav
+                }
+                "in progress" -> {
+
+                    // Set title.
+                    supportActionBar?.title = "In progress"
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.bottom_nav_fragment_container, doingFragment)
+                        .commit()
+
+                    navBar.selectedItemId = R.id.in_progress_bottom_nav
+                }
+                "done" -> {
+
+                    // Set title.
+                    supportActionBar?.title = "Done"
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.bottom_nav_fragment_container, doneFragment)
+                        .commit()
+
+                    navBar.selectedItemId = R.id.done_bottom_nav
+                }
+            }
+        }
+
+    }
+
 }
